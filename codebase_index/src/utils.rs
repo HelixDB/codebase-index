fff766026dad403fadce46d049f2b2cd6bfbbff2
Gahn Wuwong <138665743+Gahnxd@users.noreply.github.com>
2025-07-13 22:49:43 -0700

    no js

--- codebase_index/src/utils.rs
+++ codebase_index/src/utils.rs
@@ -73,7 +73,6 @@ pub fn get_language(file_path: &Path) -> Option<tree_sitter::Language> {
     let extension = file_path.extension().and_then(|s| s.to_str());
     match extension {
         Some("py") => Some(tree_sitter_python::LANGUAGE.into()),
-        Some("js") => Some(tree_sitter_javascript::LANGUAGE.into()),
         Some("rs") => Some(tree_sitter_rust::LANGUAGE.into()),
         _ => None,
     }
